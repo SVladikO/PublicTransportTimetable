95a80e53beabe792cef2f1fb1934b90b7e63c9a7
svladiko <svladiko@mail.ru>
2019-06-12 13:57:23 +0300

    [ add ] - color: green, yellow, red, blue, light-blue

--- src/js/app.js
+++ src/js/app.js
@@ -4,10 +4,40 @@ const CHARACTERS_CATALOG = require('./characters.json');
 
 let log = console.log;
 let colorCatalog = {
+	lightBlue: {
+		active: 'public/img/light_blue_on.png',
+		disabled: 'public/img/off.png'
+	},
+	blue: {
+		active: 'public/img/blue_on.png',
+		disabled: 'public/img/off.png'
+	},
+	yellow: {
+		active: 'public/img/yellow_on.png',
+		disabled: 'public/img/off.png'
+	},
+	red: {
+		active: 'public/img/red_on.png',
+		disabled: 'public/img/off.png'
+	},
+
 	green: {
-		active: 'public/img/green_on.jpg',
-		disabled: 'public/img/green_off.jpg'
-	}
+		active: 'public/img/_green_on.png',
+		disabled: 'public/img/off.png'
+	},
+	_red: {
+		active: 'public/img/_red_on.png',
+		disabled: 'public/img/off.png'
+	},
+	_blue: {
+		active: 'public/img/_blue_on.png',
+		disabled: 'public/img/off.png'
+	},
+
+	_yellow_on: {
+		active: 'public/img/_yellow_on.png',
+		disabled: 'public/img/off.png'
+	},
 }
 class InfoTable {
 	constructor(rootClass, text = '', time = 200, columns = 54, color = 'green') {
