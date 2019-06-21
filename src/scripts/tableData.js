27b84e85ac65b92d316f4dc8dd7218df1e52b386
svladiko <svladiko@mail.ru>
2019-06-21 18:11:41 +0300

    [ add ]  throw new Error('.rootClass isn't valid');

--- src/scripts/tableData.js
+++ src/scripts/tableData.js
@@ -1,9 +1,9 @@
 'use strict';
 
 let Color = require('./color');
-
 class TableData {
   constructor(rootClass, { height = 80, columns = 21, color = Color.green, language = 'eng', interval = 500 } = {}) {
+    if (!rootClass || rootClass.length === 0) throw new Error(".rootClass isn't valid");
     this.rootClass = rootClass;
     this.height = height;
     this.columns = columns;
