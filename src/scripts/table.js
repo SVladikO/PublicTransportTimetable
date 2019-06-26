d1677ed1fe339226e71b06bcba565375a973e859
svladiko <svladiko@mail.ru>
2019-06-27 02:21:22 +0300

    [ ADD ] - table.js - doc

--- src/scripts/table.js
+++ src/scripts/table.js
@@ -1,6 +1,15 @@
 'use strict';
 
 module.exports = class Table {
+  /**
+   * @param  {string} className
+   * @param  {number} {height=30  - higher text
+   * @param  {number} columns=40  - longer table
+   * @param  {string} color='chartreuse'  - text color
+   * @param  {string} language='eng'  - list of characters eng/ua
+   * @param  {number} interval=500  - timeout (needed for moveLeft/moveRight)
+   * @param  {string} backgroundColor='black' - timetable background
+   */
   constructor(className, { height = 30, columns = 40, color = 'chartreuse', language = 'eng', interval = 500, backgroundColor = 'black' } = {}) {
     if (!className || className.length === 0) throw new Error(".className isn't valid");
     this.className = className;
